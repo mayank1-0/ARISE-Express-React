f701f1b824da9930eb0b46b8a1a1a3bdd3f7fe94
Radiatechs-Mayank <103631131+Radiatechs-Mayank@users.noreply.github.com>
2023-01-03 17:28:30 +0530

    commit

---
+++ middleware/checkLogInAdmin.js
@@ -0,0 +1,15 @@
+module.exports = (req, res, next) => {
+    try {
+      if (req.session.user.name == "admin" && req.session.token) {
+        console.log("aaaa");
+        next();
+      } else {
+        console.log("bbbb");
+        res.redirect("/admin/admin-login");
+      }
+    } catch (error) {
+      console.log("cccc");
+      res.redirect("/admin/admin-login");
+    }
+  };
+  
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('../middleware/auth');
+const isLoggedInAdmin = require('../middleware/isLoggedInAdmin');
+const checkLogInAdmin = require('../middleware/checkLogInAdmin');
+const upload = require("../middleware/upload");
 var { admin, admin_login, login_admin, admin_dashboard, admin_change_password, change_password_admin, admin_logout } = require('../controller/admin_controller');
 
 /* admin routes */
 router.post('/create-admin', admin);
-router.get('/admin-login', admin_login)
+router.get('/admin-login', isLoggedInAdmin, admin_login)
 router.post('/login-admin', login_admin);
-router.get('/admin-dashboard', admin_dashboard)
-router.get('/admin-change-password', admin_change_password)
-router.put('/change-password-admin', change_password_admin)
+router.get('/admin-dashboard', checkLogInAdmin, admin_dashboard)
+router.get('/admin-change-password', checkLogInAdmin, admin_change_password)
+router.put('/change-password-admin', checkLogInAdmin, change_password_admin)
 router.get('/admin-logout', admin_logout)
 
 module.exports = router;
